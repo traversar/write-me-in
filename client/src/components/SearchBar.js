13abfce73674260dcac0b9680f5521b9588ad3dc
Andrew <travers.drew@gmail.com>
2021-08-29 13:39:58 -0400

    Search redirect bug & btn styling

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,23 +1,28 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import * as StoryActions from '../actions/stories';
 
 const SearchBar = ({
     searchQuery
 }) => {
     const [searchInput, setSearch] = useState();
+    const history = useHistory();
 
     const handleSearch = e => {
         searchQuery(searchInput);
+        console.log('here');
+        history.push('/');
     }
 
     const updateSearch = e => {
         setSearch(e.target.value)
     }
 
+
     return (
         <div className='sbar-container'>
-            <input className='sbar-input' type='text' onChange={updateSearch} placeholder='Search stories' />
+            <input className='sbar-input' type='text' onKeyPress={(e) => e.key == 'Enter' ? handleSearch() : null} onChange={updateSearch} placeholder='Search stories' />
             <button onClick={handleSearch}>Search</button>
         </div>
     )
